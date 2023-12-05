1e9e3947463d684df1fbcadf6c06a1aeb3983d35
Diva Nafisyah <diva.nafisyah.ft21@mail.umy.ac.id>
2023-12-05 21:33:40 +0700

    add function on RepositorySiswa

--- app/src/main/java/com/example/pam10/repository/RepositorySiswa.kt
+++ app/src/main/java/com/example/pam10/repository/RepositorySiswa.kt
@@ -1,4 +1,12 @@
 package com.example.pam10.repository
 
+import com.example.pam10.data.Siswa
+import kotlinx.coroutines.flow.Flow
+
 interface RepositorySiswa {
+    fun getAllSiswaStream(): Flow<List<Siswa>>
+    fun getSiswaStream(id: Int): Flow<Siswa?>
+    suspend fun insertSiswa(siswa: Siswa)
+    suspend fun deleteSiswa(siswa: Siswa)
+    suspend fun updateSiswa(siswa:Siswa)
 }
