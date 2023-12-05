c70e78ca2605cff9151b113d9fcbe04c4109592a
Diva Nafisyah <diva.nafisyah.ft21@mail.umy.ac.id>
2023-12-05 21:56:27 +0700

    add class EntryViewModel

--- app/src/main/java/com/example/pam10/model/EntryViewModel.kt
+++ app/src/main/java/com/example/pam10/model/EntryViewModel.kt
@@ -1,2 +1,55 @@
 package com.example.pam10.model
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.pam10.data.Siswa
+import com.example.pam10.repository.RepositorySiswa
+
+class EntryViewModel(private val repositoriSiswa: RepositorySiswa): ViewModel(){
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private fun validasiInput(uiState : DetailSiswa = uiStateSiswa.detailSiswa): Boolean{
+        return with(uiState){
+            nama.isNotBlank()&& alamat.isNotBlank()&& telpon.isNotBlank()
+        }
+    }
+
+    suspend fun saveSiswa(){
+        if (validasiInput()){
+            repositoriSiswa.insertSiswa(uiStateSiswa.detailSiswa.toSiswa())
+        }
+    }
+}
+data class UIStateSiswa(
+    val detailSiswa: DetailSiswa = DetailSiswa(),
+    val isEntryValid: Boolean = false
+)
+
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = "",
+)
+
+fun DetailSiswa.toSiswa(): Siswa = Siswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telepon = telpon,
+)
+
+fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
+)
+
+fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telepon,
+)
