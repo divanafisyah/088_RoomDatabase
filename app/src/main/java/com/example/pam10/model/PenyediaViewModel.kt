6413e2c60f3e043e8cea0111538771a94010240d
Diva Nafisyah <diva.nafisyah.ft21@mail.umy.ac.id>
2023-12-05 22:09:23 +0700

    add function on PenyediaViewModel

--- app/src/main/java/com/example/pam10/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pam10/model/PenyediaViewModel.kt
@@ -1,2 +1,21 @@
 package com.example.pam10.model
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.pam10.AplikasiSiswa
+
+object PenyediaViewModel{
+    val factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+    }
+}
+
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
