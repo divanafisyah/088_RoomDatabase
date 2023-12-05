c08b594579de1ee76f3420e02fe9616b790e49f6
Diva Nafisyah <diva.nafisyah.ft21@mail.umy.ac.id>
2023-12-05 21:39:00 +0700

    add function on OfflineRepositorySiswa

--- app/src/main/java/com/example/pam10/repository/OfflineRepositorySiswa.kt
+++ app/src/main/java/com/example/pam10/repository/OfflineRepositorySiswa.kt
@@ -1,4 +1,17 @@
 package com.example.pam10.repository
 
-class OfflineRepositorySiswa {
+import com.example.pam10.data.Siswa
+import com.example.pam10.data.SiswaDao
+import kotlinx.coroutines.flow.Flow
+
+class OfflineRepositorySiswa (private val siswaDao: SiswaDao): RepositorySiswa
+{
+    override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
+
+    override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
+    override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+
+    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
+
+    override suspend fun updateSiswa(siswa: Siswa) = siswaDao.update(siswa)
 }
