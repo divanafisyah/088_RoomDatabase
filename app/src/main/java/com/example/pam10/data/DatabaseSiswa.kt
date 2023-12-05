0a6d79df7900712a458e8fec7219e67c31ced055
Diva Nafisyah <diva.nafisyah.ft21@mail.umy.ac.id>
2023-12-05 21:30:01 +0700

    add function on DatabaseSiswa

--- app/src/main/java/com/example/pam10/data/DatabaseSiswa.kt
+++ app/src/main/java/com/example/pam10/data/DatabaseSiswa.kt
@@ -1,4 +1,24 @@
 package com.example.pam10.data
 
-class DatabaseSiswa {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Siswa :: class], version = 1, exportSchema = false)
+abstract class DatabaseSiswa : RoomDatabase() {
+    abstract fun siswaDao(): SiswaDao
+
+    companion object {
+        @Volatile
+        private var Instances: DatabaseSiswa? = null
+
+        fun getDatabase(context: Context): DatabaseSiswa {
+            return (Instances ?: synchronized(this) {
+                Room.databaseBuilder(context, DatabaseSiswa::class.java, "siswa_database").build()
+                    .also { Instances = it }
+            }
+                    )
+        }
+    }
 }
