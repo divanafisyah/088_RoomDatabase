1d762eb4b2fac22881bb7ce1ce56a6ef4fd0b16a
Diva Nafisyah <diva.nafisyah.ft21@mail.umy.ac.id>
2023-12-05 21:40:15 +0700

    add class on ContainerApp

--- app/src/main/java/com/example/pam10/repository/ContainerApp.kt
+++ app/src/main/java/com/example/pam10/repository/ContainerApp.kt
@@ -1,2 +1,14 @@
 package com.example.pam10.repository
 
+import android.content.Context
+import com.example.pam10.data.DatabaseSiswa
+
+interface ContainerApp{
+    val repositoriSiswa : RepositorySiswa
+}
+
+class ContainerDataApp(private val context: Context): ContainerApp{
+    override val repositoriSiswa: RepositorySiswa by lazy {
+        OfflineRepositorySiswa(DatabaseSiswa.getDatabase(context).siswaDao())
+        }
+}
