e162de14c98a663768eea5b02d496fc2db8dbb59
Diva Nafisyah <diva.nafisyah.ft21@mail.umy.ac.id>
2023-12-05 22:07:07 +0700

    add class and function on AplikasiSiswa

--- app/src/main/java/com/example/pam10/AplikasiSiswa.kt
+++ app/src/main/java/com/example/pam10/AplikasiSiswa.kt
@@ -1,4 +1,14 @@
 package com.example.pam10
 
-class AplikasiSiswa {
+import android.app.Application
+import com.example.pam10.repository.ContainerApp
+import com.example.pam10.repository.ContainerDataApp
+
+class AplikasiSiswa : Application() {
+    lateinit var container: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
 }
